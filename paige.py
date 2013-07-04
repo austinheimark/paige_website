25c8fcfc0bf9d6f391ae6af1e21a6ba9036311fa
Austin Heimark <austin@duckduckgo.com>
2013-07-04 10:56:31 -0400

    paige.py now renders the home page template

--- paige.py
+++ paige.py
@@ -3,10 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('layout.html')
+    return render_template('home.html')
 
 if __name__ == "__main__"
-    app.run()
+    app.run(debug=True)
 
 
 
