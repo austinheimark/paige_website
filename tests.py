d1bf7f540f2d262c735f6bd8dff994293cafa6f0
Austin Heimark <austin@duckduckgo.com>
2013-07-04 11:07:59 -0400

    Added one basic test

--- tests.py
+++ tests.py
@@ -1,3 +1,8 @@
 import unittest
 import pytest
+from paige import *
 
+class TestFunctionalGetRequests(unittest.TestCase):
+    def test_home_page(self):
+        check = self.app.get('/')
+        assert check == 202
