058be24b6c71da03c4a3c4a2c31bd20830dd2eee
Austin Heimark <austin@duckduckgo.com>
2013-07-09 16:43:22 -0400

    Add test against cheater cookies

--- tests.py
+++ tests.py
@@ -16,15 +16,18 @@ class TestFunctionalGetRequests(BaseClass):
 
 @pytest.mark.admin
 class TestAdminPage(BaseClass):
-    @pytest.mark.x
     def test_logged_in(self):   
         self.app.set_cookie('localhost', '9f4yZIjq', 'CsyGlIE0')
-        assert self.app.cookie_jar._cookies
         response = self.app.get('/admin')
         assert response.status_code == 200
 
-    @pytest.mark.y
     def test_not_logged_in(self):
         assert not self.app.cookie_jar._cookies
+        response = self.app.get('/admin')
+        assert response.status_code == 401
+
+    @pytest.mark.cheater
+    def test_cheater_cookie(self):
+        self.app.set_cookie('localhost', 'cheater-key', 'cheater-value')
         response = self.app.get('/admin')
         assert response.status_code == 401
