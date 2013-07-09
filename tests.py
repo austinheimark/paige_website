8af95e0fd5141e6001a7619a7819eb4f5f501d19
Austin Heimark <austin@duckduckgo.com>
2013-07-09 12:28:23 -0400

    Added tests
    It tests the app to make sure the page loads
    Minimum that a function can return is None

--- tests.py
+++ tests.py
@@ -1,10 +1,14 @@
 import unittest
 import pytest
 import requests
-from paige import *
+from paige import app
 
 class TestFunctionalGetRequests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    @pytest.mark.a
     def test_home_page(self):
-        check = requests.get('/')
-        assert check.status_code == 202
+        response = self.app.get('/')
+        assert response.status_code == 200
 
