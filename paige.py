b07d7227cb6c80c33993423074c2c9a8ca056795
Austin Heimark <austin@duckduckgo.com>
2013-07-09 17:18:00 -0400

    Added comment

--- paige.py
+++ paige.py
@@ -34,13 +34,14 @@ def sculptures():
 
 @app.route('/admin')
 def admin():
+    #if the cookie is valid, then the admin page will be shown, otherwise an abort error
     try:
         if request.cookies['9f4yZIjq'] == 'CsyGlIE0':
             return render_template('admin.html', page='ADMIN')
     except KeyError:
         pass
     abort(401)
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
