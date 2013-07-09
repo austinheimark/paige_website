520a8ae8b3146695c96de1ff30bd87585e59ebaa
Austin Heimark <austin@duckduckgo.com>
2013-07-09 12:25:05 -0400

    Added sign up button, resume as a pdf, flask templating

--- paige.py
+++ paige.py
@@ -9,27 +9,27 @@ def home():
 #route to paige's resume
 @app.route('/resume')
 def resume():
-    return render_template('paige_resume.htm')
+    return render_template('paige_resume.htm', page='RESUME')
 
 #route to paige's contact page
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', page='CONTACT')
 
 #route to paige's drawings
 @app.route('/drawings')
 def drawings():
-    return render_template('drawings.html')
+    return render_template('drawings.html', page='DRAWINGS')
 
 #route to paige's paintings
 @app.route('/paintings')
 def paintings():
-    return render_template('paintings.html')
+    return render_template('paintings.html', page='PAINTINGS')
 
 #route to paige's sculptures
 @app.route('/sculptures')
 def sculptures():
-    return render_template('sculptures.html')
+    return render_template('sculptures.html', page='SCULPTURES')
 
 if __name__ == "__main__":
     app.run(debug=True)
