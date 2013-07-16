433561a209ce286ec99d0d395c00f48fccf7cdec
Austin Heimark <austin@duckduckgo.com>
2013-07-16 10:22:06 -0400

    Added comment to explain the 401 redirect

--- paige.py
+++ paige.py
@@ -42,6 +42,7 @@ def admin():
         pass
     abort(401)
 
+#is returned when user tries to access a page that they are unauthorized to access
 @app.errorhandler(401):
 def unauthorized_page():
     return render_template('unauthorized.html'), 401
