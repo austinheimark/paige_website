8cf6341d256b0effba0695add3fa442ef3c5cd4f
Austin Heimark <austin@duckduckgo.com>
2013-08-06 18:51:04 -0400

    Added get_db()

--- paige.py
+++ paige.py
@@ -5,7 +5,9 @@ from flask import (
     abort,
     url_for,
     redirect,
-    flash
+    flash,
+    g,
+     _app_ctx_stack
     )
 
 import flask
@@ -32,6 +34,15 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()    
+
+def get_db():
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        sqlite_db.row_factory = sqlite3.Row
+        top.sqlite_db = sqlite_db
+
+    return top.sqlite_db
 
 @app.route('/')
 def home():
