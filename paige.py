b4f4a8ba3a861ff6dc225eefd62e9476d1b58236
Austin Heimark <austin@duckduckgo.com>
2013-08-06 16:42:13 -0400

    Added init_db()

--- paige.py
+++ paige.py
@@ -26,6 +26,13 @@ def verify_login():
     except KeyError:
         return False
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()    
+
 @app.route('/')
 def home():
     return render_template('home.html')
