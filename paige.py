40abf690d037cf5ac0666dfe92deec77875ccc85
austinheimark <austinheimark@gmail.com>
2013-08-21 16:07:22 -0400

    Updated the logout to work now

--- paige.py
+++ paige.py
@@ -159,10 +159,12 @@ def authenticate():
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    #logout redirects to the home page
+    # logout redirects to the home page
     response = redirect(url_for('home'))
-    #reset the cookie with the wrong value and an immediate expiration date --> hence logged out
-    #response.set_cookie(REAL_KEY, 'wrong', expires=0)
+    # reset the cookie with the wrong value and an immediate expiration date --> hence logged out
+    # don't actually need to remove the cookie from the browser but it still works since if
+    # removed from the database
+    response.set_cookie(REAL_KEY, 'wrong', expires=0)
 
     # remove session pair from the database
     session_pair = SessionPair.query.first()
